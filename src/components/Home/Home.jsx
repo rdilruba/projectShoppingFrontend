edaffe33cd8b37e7073136fbd69c133a0da45c8f
Dilruba Köse <dilrubakose@gmail.com>
2020-06-01 00:17:40 +0300

    fix

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Home.scss";
 import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
 import Layout from "../Layout/Layout";
 
 function Home() {
@@ -10,7 +9,6 @@ function Home() {
       <header className="home-header"></header>
       <Header></Header>
       <Layout></Layout>
-      <Footer></Footer>
     </div>
   );
 }
