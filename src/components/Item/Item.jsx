993e1b8e08ef383bc008ac5e1c0f7edf50b1aaab
furkanaydar <furkanaydar97@gmail.com>
2020-06-02 00:25:54 +0300

    fetch products from db

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,36 +6,49 @@ import "./Item.scss";
 const { Meta } = Card;
 const style = { width: "80%", padding: 20, marginLeft: 30 };
 
-function Item() {
-  return (
-    <div className="Item">
-      <Card
-        hoverable
-        style={style}
-        cover={
-          <img
-            alt="example"
-            src="https://d23ic3f0nw4szy.cloudfront.net/marketpaketi/products/548702/ulker-cikolata-sutlu-tablet-80-gr-a029e5f3b8ea57eb594e4496fdaba3a3.jpg"
-          />
-        }
-      >
-        <Meta
-          style={{ display: "flex", justifyContent: "center" }}
-          title="Europe Street beat"
-          description="www.instagram.com"
-        />
-        <div
-          style={{
-            marginTop: 20,
-            display: "flex",
-            justifyContent: "center",
-          }}
+class Item extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="Item">
+        <Card
+          hoverable
+          style={style}
+          cover={
+            <img
+              alt="example"
+              src={this.props.product.imgUrl}
+            />
+          }
         >
-          <Button icon={<ShoppingCartOutlined />}>Add to Cart</Button>
-        </div>
-      </Card>
-    </div>
-  );
+          <Meta
+            style={{ display: "flex", justifyContent: "center" }}
+            title={this.props.product.name}
+            description="www.instagram.com"
+          />
+          <div
+            style={{
+              marginTop: 20,
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Button icon={<ShoppingCartOutlined />}>Add to Cart</Button>
+          </div>
+        </Card>
+      </div>
+    )
+  }
 }
 
 export default Item;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,40 +1,50 @@
 import React from "react";
 import { Divider, Row, Col } from "antd";
 
+import axios from 'axios'
 import Item from "../Item/Item";
 import "./Layout.scss";
 
-function Layout() {
-  return (
-    <div className="layout-all">
-      <Divider
-        orientation="center"
-        style={{ color: "#444", fontWeight: "normal" }}
-      >
-        Ürünler
+class Layout extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      products: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://shopper496.herokuapp.com/products')
+      .then((response) => {
+        this.setState({
+          products: response.data
+        })
+      }, (error) => {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div className="layout-all">
+        <Divider
+          orientation="center"
+          style={{ color: "#444", fontWeight: "normal" }}
+        >
+          Ürünler
       </Divider>
-      <Row gutter={[48, 48]}>
-        <Col className="gutter-row" span={6}>
-          <Item></Item>
-        </Col>
-        <Col className="gutter-row" span={6}>
-          <Item></Item>
-        </Col>
-        <Col className="gutter-row" span={6}>
-          <Item></Item>
-        </Col>
-        <Col className="gutter-row" span={6}>
-          <Item></Item>
-        </Col>
-        <Col className="gutter-row" span={6}>
-          <Item></Item>
-        </Col>
-        <Col className="gutter-row" span={6}>
-          <Item></Item>
-        </Col>
-      </Row>
-    </div>
-  );
+        <Row gutter={[48, 48]}>
+          {
+            this.state.products.map((product) =>
+              <Col className="gutter-row" span={6}>
+                <Item product={product}></Item>
+              </Col>
+            )
+          }
+        </Row>
+      </div>
+    )
+  }
 }
 
 export default Layout;
