82b07b2544531de6d1101f5c9264897110cb2b23
furkanaydar <furkanaydar97@gmail.com>
2020-06-04 01:53:50 +0300

    final

--- src/components/Pay/Pay.jsx
+++ src/components/Pay/Pay.jsx
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import Header from "../Header/Header";
 import Cards from "react-credit-cards";
 import moment from 'moment'
+import BasketItem from "../BasketItem/BasketItem";
+import { Row } from "antd";
+
 
 
 import {
@@ -47,59 +50,80 @@ class Pay extends Component {
     alert(dateString.substring(0, 2) + dateString.substring(3, 5))
     this.setState({ expiry: dateString });
   }
-
+  calcCost = (product, totalCost) => {
+    totalCost += product.price * product.soldCount;
+    return totalCost;
+  };
   render() {
     const listBasket = this.props.location.listBasket ? this.props.location.listBasket : [];
     const monthFormat = "MM/YY";
+    let totalCost = 0;
 
     return (
       <div style={{ margin: "auto" }}>
         <div>
           <Header listBasket={listBasket}></Header>
         </div>
-        <div style={{ margin: "auto", backgroundColor: "red" }} id="PaymentForm">
-          <div style={{ width: "40%", float: "left" }}>
-            <Cards
-              cvc={this.state.cvc}
-              expiry={this.state.expiry}
-              name={this.state.name}
-              number={this.state.number}
-              focused={this.state.focus}
-            />
+        <div>
+          <div style={{width:"30%", float: "left"}}>
+            {
+              listBasket.map((product) => (
+                <Row
+                  style={{ display: "flex", justifyContent: "center" }}
+                  key={product.id}
+                >
+                  <BasketItem product={product}></BasketItem>
+                </Row>
+              ))}
           </div>
+          <div style={{ margin: "auto", marginLeft: 400, marginTop: 50, backgroundColor: "red" }} id="PaymentForm">
+            <div style={{ width: "40%", float: "left" }}>
+              <Cards
+                cvc={this.state.cvc}
+                expiry={this.state.expiry}
+                name={this.state.name}
+                number={this.state.number}
+                focused={this.state.focus}
+              />
+              
+            </div>
 
-          <div style={{ width: "40%", float: "left" }}>
-            <Form
-              labelCol={{ span: 4 }}
-              wrapperCol={{ span: 14 }}
-              layout="horizontal"
-            >
-              <Form.Item>
-                <InputNumber onFocus={() => this.handleFocus("number")} value={this.state.number} onChange={this.handleNumberChange}
-                  style={{ maxWidth: 300, width: 300 }} placeholder="Card Number" maxLength={22} />
-              </Form.Item>
-              <Form.Item>
-                <Input onFocus={() => this.handleFocus("name")} value={this.state.name}
-                  onChange={this.handleNameChange} style={{ maxWidth: 300, width: 300 }} placeholder="Name and Surname" />
-              </Form.Item>
+            <div style={{ width: "40%", float: "left" }}>
+              <Form
+                labelCol={{ span: 4 }}
+                wrapperCol={{ span: 14 }}
+                layout="horizontal"
+              >
+                <Form.Item>
+                  <InputNumber onFocus={() => this.handleFocus("number")} value={this.state.number} onChange={this.handleNumberChange}
+                    style={{ maxWidth: 300, width: 300 }} placeholder="Card Number" maxLength={22} />
+                </Form.Item>
+                <Form.Item>
+                  <Input onFocus={() => this.handleFocus("name")} value={this.state.name}
+                    onChange={this.handleNameChange} style={{ maxWidth: 300, width: 300 }} placeholder="Name and Surname" />
+                </Form.Item>
 
-              <Form.Item >
-                <DatePicker selected={moment(this.state.expiry)} onFocus={() => this.handleFocus("expiry")}
-                  onChange={this.handleDateChange} style={{ maxWidth: 300, width: 300 }} placeholder="Expiry Date" format={monthFormat} picker="month" />
-              </Form.Item>
+                <Form.Item >
+                  <DatePicker selected={moment(this.state.expiry)} onFocus={() => this.handleFocus("expiry")}
+                    onChange={this.handleDateChange} style={{ maxWidth: 300, width: 300 }} placeholder="Expiry Date" format={monthFormat} picker="month" />
+                </Form.Item>
 
-              <Form.Item>
-                <InputNumber onFocus={() => this.handleFocus("cvc")} value={this.state.cvc}
-                  onChange={this.handleCvcChange} style={{ maxWidth: 300, width: 300 }} placeholder="CVC" maxLength={3} />
-              </Form.Item>
-
-              <Form.Item>
-                <Button>
-                  <CreditCardOutlined />
+                <Form.Item>
+                  <InputNumber onFocus={() => this.handleFocus("cvc")} value={this.state.cvc}
+                    onChange={this.handleCvcChange} style={{ maxWidth: 300, width: 300 }} placeholder="CVC" maxLength={3} />
+                </Form.Item>
+                <div style={{ margin: 'auto', marginBottom:20, fontSize: 28, fontWeight: "lighter" }}>
+          {'You will be paying 5000 $'} 
+            </div>
+                <Form.Item>
+                  <Button>
+                    <CreditCardOutlined />
                   Order
                   </Button>
-              </Form.Item>
-            </Form>
+                </Form.Item>
+              </Form>
+              
+            </div>
           </div>
         </div>
       </div>
